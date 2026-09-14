4bb96cbeb5864a84aa51eb6e7885cfca9c516b32
Finoanaa <hei.finoana.8@gmail.com>
2026-09-14 15:53:02 +0200

    feat: remove treatments from sidebar

--- src/components/Sidebar/menuItems.js
+++ src/components/Sidebar/menuItems.js
@@ -2,7 +2,6 @@ import {
     FaHome,
     FaUserFriends,
     FaCalendarAlt,
-    FaTooth,
     FaCog,
     FaSignOutAlt
 } from "react-icons/fa";
@@ -11,7 +10,6 @@ const menuItems = [
     { title: "Dashboard", icon: FaHome, path: "/" },
     { title: "Patients", icon: FaUserFriends, path: "/patients" },
     { title: "Rendez-vous", icon: FaCalendarAlt, path: "/appointments" },
-    { title: "Traitements", icon: FaTooth, path: "/treatments" },
     { title: "Paramètres", icon: FaCog, path: "/settings" }
 ];
 
