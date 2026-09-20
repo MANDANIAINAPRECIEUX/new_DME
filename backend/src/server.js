58cf0c2492f455f679e9c0942876935da36a52f0
unknown <mandaniainaprecieux@yahoo.com>
2026-09-20 22:09:00 +0200

    feat(backend): add Express server and health endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const port = Number(process.env.PORT || 3000);
 
+// il faut juste ces 4 lignes pour creer un serveur
+
+const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
   console.log(`Backend démarré sur http://localhost:${port}`);
 });
